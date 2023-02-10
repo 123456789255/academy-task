d0b5c93c407feb7fdb3d0265731e993692f49eaa
Your Name <you@example.com>
2023-02-09 22:57:58 -0800

    three commit

--- pukvjpi-m1/routes/web.php
+++ pukvjpi-m1/routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'welcome'])->name('welcome');
+Route::get('/', [App\Http\Controllers\IndexController::class, 'welcome'])->name('about');
 
 Auth::routes();
 
